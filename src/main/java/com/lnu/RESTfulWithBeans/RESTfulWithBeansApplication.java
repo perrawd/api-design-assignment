64c1ecb74bad12a233b1b009768c21b6f4f02377
Per Rawdin <pr222iw@student.lnu.se>
2022-01-24 20:32:48 +0100

    Add framework and dependencies

---
+++ src/main/java/com/lnu/RESTfulWithBeans/RESTfulWithBeansApplication.java
@@ -0,0 +1,13 @@
+package com.lnu.RESTfulWithBeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulWithBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulWithBeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lnu/RESTfulWithBeans/RESTfulWithBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnu.RESTfulWithBeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RESTfulWithBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
