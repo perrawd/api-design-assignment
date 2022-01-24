06af53c6edc0f1ed732d6702b87af8be6a9b9f66
Per Rawdin <pr222iw@student.lnu.se>
2022-01-24 22:45:20 +0100

    Add framework and dependencies

---
+++ src/main/java/com/lnu/RESTfulCafe/RESTfulCafeApplication.java
@@ -0,0 +1,13 @@
+package com.lnu.RESTfulCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulCafeApplication.class, args);
+	}
+
+}
