a6250a837d9fd434fe56e8c86e0c7a13150340dd
Per Rawdin <pr222iw@student.lnu.se>
2022-01-27 13:41:09 +0100

    Add SecurityConfig

--- src/main/java/com/lnu/RESTfulCafe/RESTfulCafeApplication.java
+++ src/main/java/com/lnu/RESTfulCafe/RESTfulCafeApplication.java
@@ -2,9 +2,17 @@ package com.lnu.RESTfulCafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RESTfulCafeApplication {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RESTfulCafeApplication.class, args);
