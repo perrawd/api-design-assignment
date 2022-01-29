b8e6890c01020b999a0d3215ed5fc3fae46cd7d3
Per Rawdin <pr222iw@student.lnu.se>
2022-01-29 20:39:43 +0100

    Update EventHandler to manage Webhook subscribers

--- src/main/java/com/lnu/RESTfulCafe/event/BeanAddedEventHandler.java
+++ src/main/java/com/lnu/RESTfulCafe/event/BeanAddedEventHandler.java
@@ -2,12 +2,14 @@ package com.lnu.RESTfulCafe.event;
 
 import com.lnu.RESTfulCafe.model.bean.Bean;
 
+import com.lnu.RESTfulCafe.model.subscriber.Event;
+import com.lnu.RESTfulCafe.model.subscriber.Subscriber;
+import com.lnu.RESTfulCafe.model.subscriber.SubscriberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,18 +17,23 @@ import java.util.List;
 
 @Component
 public class BeanAddedEventHandler {
-
-    @Value("${app.webhook.url}")
-    private String postResource;
+    @Autowired
+    private SubscriberRepository repository;
 
     @EventListener
-    public String handleEvent (BeanAddedEvent event) {
+    public void handleEvent (BeanAddedEvent event) {
+        List<Subscriber> subscribers = repository.findAll();
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         HttpEntity<Bean> entity = new HttpEntity<Bean>(event.getBean(),headers);
 
-        return restTemplate.exchange(
-                postResource, HttpMethod.POST, entity, String.class).getBody();
+        subscribers
+                .stream()
+                .filter(subscriber -> subscriber.getEvent() == Event.NEWBEAN)
+                .forEach(subscriber -> {
+                    restTemplate.exchange(subscriber.getUrl(), HttpMethod.POST, entity, Bean.class);
+                });
     }
 }
