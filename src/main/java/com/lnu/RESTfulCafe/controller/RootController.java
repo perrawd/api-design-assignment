1c4c8c05deaa64a27ec55afe28f149fb1a6b6f8e
Per Rawdin <pr222iw@student.lnu.se>
2022-01-31 19:15:23 +0100

    Add RootController

---
+++ src/main/java/com/lnu/RESTfulCafe/controller/RootController.java
@@ -0,0 +1,34 @@
+package com.lnu.RESTfulCafe.controller;
+
+import com.lnu.RESTfulCafe.model.link.NavLink;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public CollectionModel<NavLink> root() {
+        ArrayList<NavLink> links = new ArrayList<>();
+
+        NavLink beansLink = new NavLink();
+        beansLink.name = "beans";
+        beansLink.description = "The best and high quality coffee beans from around the world.";
+        beansLink.add(linkTo(methodOn(BeanController.class).all()).withRel("beans"));
+        links.add(beansLink);
+
+        NavLink drinksLink = new NavLink();
+        drinksLink.name = "Drinks";
+        drinksLink.description = "The best and high quality coffee drinks made with ❤️";
+        drinksLink.add(linkTo(methodOn(BeanController.class).all()).withRel("drinks"));
+        links.add(drinksLink);
+
+        return CollectionModel.of(links);
+    }
+}
