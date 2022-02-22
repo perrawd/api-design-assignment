52cbae09128577c4650dbd99b1b34eeec9904861
Per Rawdin <pr222iw@student.lnu.se>
2022-02-22 09:33:04 +0100

    Add CustomerModelAssembler and CustomerRepository

---
+++ src/main/java/com/lnu/RESTfulCafe/model/customer/CustomerModelAssembler.java
@@ -0,0 +1,18 @@
+package com.lnu.RESTfulCafe.model.customer;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class CustomerModelAssembler implements RepresentationModelAssembler<Customer, EntityModel<Customer>> {
+    @Override
+    public EntityModel<Customer> toModel(Customer customer) {
+        return EntityModel.of(customer,
+                linkTo(methodOn(CustomerController.class).one(customer.getId().toString())).withSelfRel(),
+                linkTo(methodOn(CustomerController.class).all()).withRel("customers"));
+    }
+}
